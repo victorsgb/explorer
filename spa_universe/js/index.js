e249f78904723b1e8421bb1b26bbb7c1d4b39d9e
Victor Baptista <victorsgb@gmail.com>
2022-07-01 17:13:18 -0300

    Background image fixed

--- spa_universe/js/index.js
+++ spa_universe/js/index.js
@@ -2,9 +2,9 @@ import Router from "./router.js"
 
 const router = new Router(); 
 
-router.add("/", "/pages/home.html", "url(images/mountains-universe-1.png)");
-router.add("/universe", "/pages/universe.html", "url(images/mountains-universe-2.png)");
-router.add("/explore", "/pages/explore.html", "url(images/mountains-universe-3.png)");
+router.add("/", "/pages/home.html");
+router.add("/universe", "/pages/universe.html");
+router.add("/explore", "/pages/explore.html");
 
 router.handle();
 
--- spa_universe/js/router.js
+++ spa_universe/js/router.js
@@ -1,9 +1,9 @@
 export default class Router {
 
-    routes = {404: ["/pages/404.html", "url(images/mountains-universe-1.png)"]};
+    routes = {404: "/pages/404.html"};
 
-    add(pathname, page, image) {
-        this.routes[pathname] = [page, image];
+    add(pathname, page) {
+        this.routes[pathname] = page;
     }
 
     route(event) {
@@ -18,12 +18,8 @@ export default class Router {
     handle() {
         const pathname = window.location.pathname;
 
-        const route = this.routes.hasOwnProperty(pathname)? this.routes[pathname][0] : this.routes[404][0];
+        const route = this.routes[pathname] || this.routes[404];
         
-        const bg_image = this.routes.hasOwnProperty(pathname)? this.routes[pathname][1] : this.routes[404][1];
-
-        document.querySelector(":root").children[1].style.setProperty("background-image", bg_image);
-
         fetch(route)
         .then(data => data.text())
         .then(html => {
